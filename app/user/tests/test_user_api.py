af92078df3d40de52f208b634491541a1b65a3c8
mobad <mostafa.m.badawey@gmail.com>
2020-08-14 17:53:35 +0200

    [TEST]: add user tests.

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,59 @@
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test the users api(public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        """Test creating user with valid payload is successful"""
+        payload = {
+            'email': 'user@user.com',
+            'password': 'user123',
+            'name': 'user'
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        """Test creating user that already exists fails"""
+        payload = {
+            'email': 'test@test.com',
+            'password': 'test',
+        }
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test that the password must be more than 5 chars"""
+        payload = {
+            'email': 'test@test.com',
+            'password': 'pw'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertTrue(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
