a3ae6c05576869b5e605bac054baffa1c6b8235f
mobad <mostafa.m.badawey@gmail.com>
2020-08-17 16:47:46 +0200

    [FEAT]: add transactions calculations and validations logic.

--- app/transaction/views.py
+++ app/transaction/views.py
@@ -21,6 +21,27 @@ def validate_created_by(value):
     if user.user_type != 'Admin':
         raise ValidationError('created_by id must belong to Admin user.')
 
+def validate_in_transaction_enough_money_in_store(amount, store):
+    return store.cash >= amount
+
+def validate_out_transaction_enough_quantity_in_store(quantity, store_product):
+    if int(store_product.quantity) >= int(quantity):
+        pass
+    else:
+        raise SuspiciousOperation()
+
+def validate_trx_type_calculations(amount, store, trx_type):
+    if trx_type == 'IN':
+        return validate_in_transaction_enough_money_in_store(amount, store)
+
+    return True
+
+def validate_transaction_calculations(amount, quantity, product, store, trx_type):
+    if float(amount) == int(quantity) * float(product.price) and validate_trx_type_calculations(float(amount), store, trx_type):
+        return True
+    else:
+        return False
+
 
 class TransactionValidationForm(forms.Form):
     created_by = forms.IntegerField(validators=[validate_created_by])
@@ -41,12 +62,14 @@ class TransactionViewSet(viewsets.ModelViewSet):
 
     @transaction.atomic
     def perform_create(self, serializer):
+        # Validate Form Data
         data = TransactionValidationForm(self.request.data)
         if not data.is_valid():
-            raise SuspiciousOperation('Invalid Data')
+            raise SuspiciousOperation()
         else:
             pass
 
+        # extract request body data
         created_by = User.objects.get(pk=self.request.data['created_by'])
         party_id = User.objects.get(pk=self.request.data['party_id'])
         store_id = Store.objects.get(pk=self.request.data['store_id'])
@@ -55,6 +78,13 @@ class TransactionViewSet(viewsets.ModelViewSet):
         amount = self.request.data['amount']
         quantity = self.request.data['quantity']
 
+        # Validate transaction calculations
+        if validate_transaction_calculations(amount=amount, quantity=quantity, product=product_id, store=store_id, trx_type=trx_type):
+            pass
+        else:
+            raise SuspiciousOperation()
+
+        # Start transaction
         try:
             with transaction.atomic():
                 trx = Transaction(
@@ -82,19 +112,26 @@ class TransactionViewSet(viewsets.ModelViewSet):
                     store_id=store_id,
                     product_id=product_id
                 )
+                    validate_out_transaction_enough_quantity_in_store(quantity=quantity, store_product=store_product)
+
                     if trx_type == 'IN':
-                        store_product.quantity += quantity
+                        store_product.quantity += int(quantity)
                     else:
-                        store_product.quantity -= quantity
+                        store_product.quantity -= int(quantity)
 
                     store_product.save()
                 else:
-                    store_product = StoreProduct(
-                        store_id=store_id,
-                        product_id=product_id,
-                        quantity=quantity,
-                    )
-                    store_product.save()
+                    if trx_type == 'OUT':
+                        raise SuspiciousOperation()
+
+                    else:
+                        store_product = StoreProduct(
+                            store_id=store_id,
+                            product_id=product_id,
+                            quantity=quantity,
+                        )
+                        store_product.save()
+
 
         except IntegrityError:
             print('Something Went Wrong in Transactions View atomic handler.')
