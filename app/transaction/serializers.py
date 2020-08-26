78c47cf71c27c5979f82153fa23005127ff2792f
mobad <mostafa.m.badawey@gmail.com>
2020-08-26 21:02:36 +0200

    [REFACTOR]: serialize trx store and party objects.

--- app/transaction/serializers.py
+++ app/transaction/serializers.py
@@ -1,12 +1,22 @@
 from rest_framework import serializers
 
-from core.models import Transaction
+from core.models import Transaction, Store
+from store.serializers import StoreSerializer
+from user.serializers import UserSerializer
 
 class TransactionSerializer(serializers.ModelSerializer):
     """Serializes Transaction objects"""
 
+    store = StoreSerializer(
+        many=False,
+        read_only=True
+    )
+    party = UserSerializer(
+        many=False,
+        read_only=True
+    )
+
     class Meta:
         model = Transaction
-        fields = ('id', 'trx_type','store_id', 'created_by', 'party_id', 'amount', 'created_at')
-        read_only_fields = ('id', 'trx_type','store_id', 'created_by', 'party_id', 'amount', 'created_at')
-
+        fields = ('id', 'trx_type','store', 'created_by', 'party', 'amount', 'created_at')
+        read_only_fields = ('id', 'trx_type','store', 'created_by', 'party', 'amount', 'created_at')
